2a598f0941e0f53b3a60056eab6a72f67c144d03
spencertlogan <100391698+spencertlogan@users.noreply.github.com>
2022-10-01 14:06:33 -0500

    Initialized Player

--- classes.py
+++ classes.py
@@ -1,3 +1,15 @@
+class Player:
+    
+    def __init__(self, xpos, ypos, health, speed, width, height, img):
+        self.xpos = xpos
+        self.ypos = ypos
+        self.health = health
+        self.speed = speed
+        self.width = width
+        self.height = height
+        self.img = img
+        
+        
 class Obstacle:
 
     def __init__(self, xpos, ypos):
--- main.py
+++ main.py
@@ -5,9 +5,27 @@ pygame.init()
 WINDOW = pygame.display.set_mode((500, 600))
 BLACK = (0, 0, 0)
 
+#initialize player variables
+playerHealth = 100
+playerSpeed = 0
+playerHeight = 50
+playerWidth = 50
+playerXpos = (WINDOW.get_width() / 2) - (playerWidth / 2)
+playerYpos = (WINDOW.get_height() / 2) - (playerHeight/2)
+playerImg = pygame.image.load("Alien.png")
+playerImg = pygame.transform.scale(playerImg, (playerWidth, playerHeight))
+
+player = classes.Player(playerXpos, playerYpos, playerHealth, playerSpeed, playerWidth, playerHeight, playerImg)
+
+
+
+
+
 running = True
 while running:
     WINDOW.fill(BLACK)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    WINDOW.blit(player.img, (player.xpos, player.ypos))
+    pygame.display.update()
