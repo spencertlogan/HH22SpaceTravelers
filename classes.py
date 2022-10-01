01c0177739bac001965c1b9a9c86406b2e179b9e
xProioxis <christionb@hotmail.com>
2022-10-01 13:23:19 -0500

    added classes

---
+++ classes.py
@@ -0,0 +1,13 @@
+class Obstacle:
+
+    def __init__(self, xpos, ypos):
+        self.xpos = xpos
+        self.ypos = ypos
+
+class Rectangle(Obstacle):
+
+    def __init__(self, xpos, ypos, len, width):
+        self.xpos = xpos
+        self.ypos = ypos
+        self.len = len
+        self.width = width
--- main.py
+++ main.py
@@ -1,18 +1,9 @@
 import pygame
+import classes
 
 pygame.init()
 WINDOW = pygame.display.set_mode((500, 600))
 BLACK = (0, 0, 0)
-
-class Obstacle:
-    
-    def __init__(self, xpos, ypos, img=None):
-        self.xpos = xpos
-        self.ypos = ypos
-        self.img = img
-
-    
-
 
 running = True
 while running:
