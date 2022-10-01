a18981d3fc8bbca6f1facae15ab72502d682d227
xProioxis <christionb@hotmail.com>
2022-10-01 13:15:01 -0500

    Added Obstacle Class

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import pygame
 
 pygame.init()
-WINDOW = pygame.display.set_mode((800, 600))
+WINDOW = pygame.display.set_mode((500, 600))
 BLACK = (0, 0, 0)
+
+class Obstacle:
+    
+    def __init__(self, xpos, ypos, img=None):
+        self.xpos = xpos
+        self.ypos = ypos
+        self.img = img
+
+    
+
 
 running = True
 while running:
